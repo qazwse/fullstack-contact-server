53c38dc040c9ec7673a6c2dc0e9081f493903f7d
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-06-12 16:20:20 -0400

    Done 3.1 - barebones server for returning contacts

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const contacts = {
+    "persons": [
+        {
+            "name": "Arto Hellas",
+            "number": "040-123456",
+            "id": 1
+        },
+        {
+            "name": "Ada Lovelace",
+            "number": "39-44-5323523",
+            "id": 2
+        },
+        {
+            "name": "Dan Abramov",
+            "number": "12-43-234345",
+            "id": 3
+        },
+        {
+            "name": "Mary Poppendieck",
+            "number": "39-23-6423122",
+            "id": 4
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send('<h1>Index</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(contacts)
+})
+
+
+const port = 3001
+app.listen(port, () => { 
+    console.log(`Server running on port ${port}`)
+})
