2ac476a5b94954465459c2e2e162ad8a43737df1
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-07-06 16:23:32 -0400

    Get contacts from DB instead of hardcoded

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
+const Contact = require('./models/contact')
+    
 morgan.token('body', function getBody(req) {
     const body = req.body
     if (Object.getOwnPropertyNames(body).length > 0) {
@@ -10,36 +13,11 @@ morgan.token('body', function getBody(req) {
     }
 })
 
-
 const app = express()
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-
-let contacts = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
 
 const getNewID = () => {
     const maxContacts = 10000
@@ -64,7 +42,9 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(contacts)
+    Contact.find({}).then(c => {
+        res.json(c)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
